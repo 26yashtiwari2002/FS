1a99678d8a357ff7ed2327b4dc6c70ec90df9eea
YASH Tiwari <92349008+26yashtiwari2002@users.noreply.github.com>
2025-08-11 21:21:09 +0530

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import storage from '../middleware/upload.js';
-import {UploadController,downloadController} from '../Controller/uploadController.js';
+import {UploadController,downloadController} from '../controller/uploadController.js';
 const router = express.Router();
 router.post('/upload', storage.single('file'), UploadController);
 router.get('/files/:fileId', downloadController);
